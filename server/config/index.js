b22b7a07344a92e289f5a2969271f45e5e86856c
aalgharibawi@gmail.com <aalgharibawi@gmail.com>
2023-11-18 18:53:44 -0500

    Post issue solved

--- server/config/index.js
+++ server/config/index.js
@@ -1,11 +1,9 @@
-require('dotenv').config();
+require("dotenv").config();
 
 //Store username and password to variables from .env file
 const username = process.env.USER;
 const password = process.env.PASSWORD;
 
-const MONGODB_URL = `mongodb+srv://${username}:${password}@connecthub.xoa3wfo.mongodb.net/?retryWrites=true&w=majority`;
-
-
+const MONGODB_URL = `mongodb+srv://${username}:${password}@connecthub.xoa3wfo.mongodb.net/users?retryWrites=true&w=majority`;
 
 module.exports = MONGODB_URL;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-require('dotenv').config();
-const userRoutes = require('./routes/userRoutes');
-const mongoose = require('mongoose');
-const MONGODB_URL = require('./config/index');
+const express = require("express");
+require("dotenv").config();
+const userRoutes = require("./routes/userRoutes");
+const mongoose = require("mongoose");
+const MONGODB_URL = require("./config/index");
 
 // Initialize express app
 const app = express();
@@ -11,21 +11,21 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+app.get("/", (req, res) => {
+  res.json({ massage: " Welcome to ConnectHub!" });
+});
 // Connect to MongoDB
-mongoose.connect(MONGODB_URL).then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB', err));
-
-
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
 
 //Use user routes
-app.use('/api/users', userRoutes);
-app.use('/auth', userRoutes);
+app.use("/", userRoutes);
+// app.use("/auth", userRoutes);
 
+const PORT = process.env.PORT || 3000;
 
-
-// start server
-app.listen(process.env.PORT, () => {
-    console.log(`Server listening on port ${process.env.PORT}.`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
